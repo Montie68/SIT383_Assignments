2f4421cf35601a79ca830c89b55ea4c91383645d
Unknown <geoff@crystalclearit.com.au>
2020-03-15 14:32:09 +1100

    App 1 completed

---
+++ Week01/App1/Assets/Scripts/PhysicalCameraTexture.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PhysicalCameraTexture : MonoBehaviour
+{
+
+    public Material camTextMaterial;
+    public TextMeshProUGUI message;
+    private WebCamTexture camTexture;
+
+    private int currentCammera = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTexture = new WebCamTexture();
+        camTextMaterial.mainTexture = camTexture;
+        camTexture.Play();
+        ShowCameras();
+    }
+
+    void Update()
+    {
+        // message.text = "Update: " + Time.time;
+    }
+
+    private void ShowCameras()
+    {
+        message.text = "";
+        foreach (WebCamDevice d in WebCamTexture.devices)
+        {
+            message.text += d.name + (d.name == camTexture.deviceName ? " *" : "") + "\n";
+        }
+    }
+
+    public void NextCamera()
+    {
+        currentCammera = (currentCammera + 1) % WebCamTexture.devices.Length;
+        // Change camera only works after stopping the current cameras feed
+
+        camTexture.Stop();
+        camTexture.deviceName = WebCamTexture.devices[currentCammera].name;
+        camTexture.Play();
+        ShowCameras();
+    }
+}
---
+++ Week01/App1/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotator = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("The Roatrion class has been Called");
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(.5f, rotator);
+    }
+}
