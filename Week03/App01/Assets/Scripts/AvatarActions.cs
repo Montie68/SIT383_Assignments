596732add20ef94b9eab6e300a39b248927a78eb
Unknown <geoff@crystalclearit.com.au>
2020-03-29 15:39:48 +1100

    Week01.app finished

--- Week03/App01/Assets/Scripts/AvatarActions.cs
+++ Week03/App01/Assets/Scripts/AvatarActions.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class AvatarActions : MonoBehaviour
+using UnityEngine.EventSystems;
+using Photon.Pun;
+public class AvatarActions : MonoBehaviourPun
 {
     [Tooltip("The prefab for objects created when the drop event is triggered.")]
     public GameObject blockTemplate;
@@ -11,23 +13,66 @@ public class AvatarActions : MonoBehaviour
     [Tooltip("Movement speed in meters per second (assumes 1 unit = 1 meter)")]
     public float moveSpeed = 10.0f;
 
+    public Vector3 SpawnArea = new Vector3(10, .25f, 10);
+
     private float turn = 0.0f;
 
     private float move = 0.0f;
+    
+    Rigidbody rb;
+    
+    void Start()
+    {
+        setButtonCallbacks();
 
+        // to stop the player avatar falling on spawn
+        rb = this.GetComponent<Rigidbody>();
+        if (!rb.isKinematic) rb.isKinematic = true;
+        if (rb.useGravity) rb.useGravity = false;
+        StartCoroutine(Spawn());
+    }
+
+    private void Awake()
+    {
+        // set the players spawn post
+        transform.SetPositionAndRotation(new Vector3(Random.Range(-SpawnArea.x, SpawnArea.x),
+                                            transform.position.y, 
+                                            Random.Range(-SpawnArea.z, SpawnArea.z)), transform.rotation);
+    }
+
+    IEnumerator Spawn()
+    {
+        // switch player rigidbody on to become interactable
+        yield return new WaitForSeconds(1);
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
     public void Update()
     {
-        transform.rotation *= Quaternion.AngleAxis(turn* turnSpeed * Time.deltaTime, Vector3.up);
-        transform.position += move* moveSpeed * transform.forward * Time.deltaTime;
+        if (photonView.IsMine == true || PhotonNetwork.IsConnected == false)
+        {
+            transform.rotation *= Quaternion.AngleAxis(turn * turnSpeed * Time.deltaTime, Vector3.up);
+            transform.position += move * moveSpeed * transform.forward * Time.deltaTime;
+            photonView.transform.position = transform.position;
+            photonView.transform.rotation = transform.rotation;
+        }
+        else
+            {
+                transform.Find("head_H/Main Camera").gameObject.SetActive(false);
+            }
     }
     private void dropObject()
     {
-        // Create the new object up and to the front, so it is away from the avatar.
-        GameObject o = Instantiate(blockTemplate, transform.position + transform.forward +
-                        transform.up, Quaternion.identity);
+        if (photonView.IsMine == true || PhotonNetwork.IsConnected == false)
+        {
+            // Create the new object up and to the front, so it is away from the avatar.
+            GameObject o = PhotonNetwork.Instantiate(blockTemplate.name, photonView.transform.position + photonView.transform.forward +
+                        photonView.transform.up, Quaternion.identity);
+        }
     }
     public void Left()
     {
+
         turn = -1.0f;
     }
     public void Right()
@@ -46,5 +91,25 @@ public class AvatarActions : MonoBehaviour
     {
         turn = 0.0f;
         move = 0.0f;
+    }
+
+    private void setButtonCallbacks()
+    {
+        // Assumes each button has already been set with two triggers; a pointer down followed 	by a pointer up.
+        GameObject.Find("Canvas/ButtonTurnL").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { Left(); });
+
+        GameObject.Find("Canvas/ButtonTurnL").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { Stop(); });
+
+        GameObject.Find("Canvas/ButtonTurnR").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { Right(); });
+
+        GameObject.Find("Canvas/ButtonTurnR").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { Stop(); });
+
+        GameObject.Find("Canvas/ButtonUp").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { Forward(); });
+
+        GameObject.Find("Canvas/ButtonUp").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { Stop(); });
+
+        GameObject.Find("Canvas/ButtonDrop").GetComponent<EventTrigger>().triggers[0].callback.AddListener((data) => { Drop(); });
+
+        GameObject.Find("Canvas/ButtonDrop").GetComponent<EventTrigger>().triggers[1].callback.AddListener((data) => { Stop(); });
     }
 }
