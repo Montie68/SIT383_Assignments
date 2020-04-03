1b554212c32b2791c6f8ffcd5ac296aeef8f1358
Unknown <geoff@crystalclearit.com.au>
2020-04-03 17:14:03 +1100

    Week 04 App 01 Finished

---
+++ Week04/Week04App01/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    // audio source to play
+    public AudioSource audioSource;
+    Canvas canvas;
+    bool hasPlayed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get audio source if not set
+        if (audioSource == null) audioSource = this.GetComponent<AudioSource>();
+        canvas = this.GetComponent<Canvas>();
+    }
+    // Play sound on enable
+    
+    void Update()
+    {
+        // test if canvas is enabled recently an play the sound.
+        if (canvas.enabled == true && !hasPlayed)
+        {
+            audioSource.Play();
+            hasPlayed = true;
+        }
+        else if (canvas.enabled == false && hasPlayed)
+        {
+            audioSource.Stop();
+            hasPlayed = false;
+        }
+    }
+
+}
