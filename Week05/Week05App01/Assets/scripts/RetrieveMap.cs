e93f2e0a173382d5037abb96673e011b46c0accf
Geoff Newman <geoff@dropbeargames.com.au>
2020-04-09 12:01:32 +1000

    Added retieve Map

---
+++ Week05/Week05App01/Assets/scripts/RetrieveMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetrieveMap : MonoBehaviour
+{
+    public int zoom = 0;
+    public int x = 0;
+    public int y = 0;
+
+    public Material mapMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
