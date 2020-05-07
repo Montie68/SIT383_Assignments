460ff9e94b4f4a562b277eadcd61578f22ecb437
geoff <geoff.newman@immersive.video>
2020-05-08 00:59:50 +1000

    Eod 07/05/2020

---
+++ Week07/week07App01/Assets/scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += v * transform.forward * Time.deltaTime * 10.0f;
+        transform.rotation *= Quaternion.AngleAxis(h * 250.0f * Time.deltaTime, transform.up);
+
+    }
+}
---
+++ Week07/week07App01/Assets/scripts/ShowPhysicalCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPhysicalCamera : MonoBehaviour
+{
+    public Camera camera;
+    public Material PhysicalMaterial;
+    private WebCamTexture webCamTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camera == null) camera = Camera.main;
+        webCamTexture = new WebCamTexture();
+        PhysicalMaterial.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pos = (camera.nearClipPlane+0.5f);
+        transform.position = camera.transform.position + camera.transform.forward * pos;
+        //transform.rotation = camera.transform.rotation;
+        float hieght = Mathf.Tan(camera.fieldOfView * Mathf.Deg2Rad * 0.5f ) * pos * 2.0f;
+        transform.localScale = new Vector3(hieght * camera.aspect, hieght, 1.0f);
+
+    }
+}
