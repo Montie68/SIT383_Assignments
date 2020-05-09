e802727622d81a8d6931e0cf3c34dd11d7fee0c1
geoff <geoff.newman@immersive.video>
2020-05-10 01:38:00 +1000

    Week 8 app Spatial Sound Complete

---
+++ Week08/Week08App01/Assets/scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        //add the player animator
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v > 0) anim.SetBool("isMovingForward", true);
+        else if (v < 0) anim.SetBool("isMovingBack", true);
+        else if (h > 0) anim.SetBool("isMovingRight", true);
+        else if (h < 0) anim.SetBool("isMovingLeft", true);
+
+        if (v == 0)
+        {
+            anim.SetBool("isMovingForward", false);
+            anim.SetBool("isMovingBack", false);
+        }
+        if (h == 0) {
+            anim.SetBool("isMovingRight", false);
+            anim.SetBool("isMovingLeft", false);
+        }
+
+
+
+        transform.position += v * transform.forward * Time.deltaTime * 10.0f;
+        transform.rotation *= Quaternion.AngleAxis(h * 250.0f * Time.deltaTime, transform.up);
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
---
+++ Week08/Week08App01/Assets/scripts/SpatialSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialSound : MonoBehaviour
+{
+    public List<GameObject> soundSources;
+    public GameObject actor;
+    public float decayFactor = 1.0f;
+    public float dropOff = 0.1f;
+
+    private float[] lastDistance;
+    private void Start()
+    {
+        lastDistance = new float[soundSources.Count];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < soundSources.Count; i++)
+        {
+            float distance = (soundSources[i].transform.position - actor.transform.position).magnitude;
+            
+            soundSources[i].GetComponent<AudioSource>().volume = 1.0f / Mathf.Pow(distance * dropOff, decayFactor);
+
+            float s = (distance - lastDistance[i]) / Time.deltaTime;
+            float c = 330.0f;
+            soundSources[i].GetComponent<AudioSource>().pitch = (c + s) / (c - s);
+            lastDistance[i] = distance;
+        }
+    }
+}
